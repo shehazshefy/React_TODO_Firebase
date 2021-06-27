81d090b22f6228d122d3a902f16ff523a993c2f6
Shehaz Shefy <shehazshefy97@gmail.com>
2021-06-27 19:59:35 +0530

    1. created a todo form and display the inputs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Take dogs for a walk', 'Take the garbage out', 'Hello everyone']);
+  const [input, setInput] = useState('');
+
+  const addTodo = (e) => {
+    //this will be executed when we click the button
+    // console.log('👽', 'working');
+    e.preventDefault(); //will prevent REFRESH
+    setTodos([...todos, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello 🚀</h1>
+      <form>
+        <input value={input} onChange={(e) => setInput(e.target.value)}/>
+        <button type='submit' onClick={addTodo}>Add Todo</button>
+      </form>
+      
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
